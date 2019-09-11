25ab32a4496541f72e5d8019da65ad516b271ad5
happyandhappy <greyfrapps@gmail.com>
2019-09-10 17:42:47 -0700

    Revert "change the index.php"
    This reverts commit fa06be3cc775c0ca4c20bba517a18fd7a94222b2.

--- index.php
+++ index.php
@@ -1,333 +1,141 @@
-<?php
-    require_once ('soapclient/SforceEnterpriseClient.php');
-    $numArray = ['double','int','boolean'];
+<?php 
+    require_once ('soapclient/SforceEnterpriseClient.php'); 
+    $Subject = "";
+    $OwnerId = NULL;
+    if(isset($_GET['PrimaryPhone'])){$PrimaryPhone = $_GET['PrimaryPhone'];}
+    if(isset($_GET['Disposition'])){$Disposition = $_GET['Disposition'];}
+    if(isset($_GET['Notes'])){$Notes = $_GET['Notes'];}
+    if(isset($_GET['Call_Recording'])){$Call_Recording = $_GET['Call_Recording'];}
+    if(isset($_GET['CallDurationInSeconds'])){$CallDurationInSeconds = $_GET['CallDurationInSeconds'];}
+    if(isset($_GET['WhoId'])){$WhoId = $_GET['WhoId'];}
+    if(isset($_GET['FirstName'])){$FirstName = $_GET['FirstName'];}
+    if(isset($_GET['LastName'])){$LastName = $_GET['LastName'];}
+    if(isset($_GET['OwnerId'])){$OwnerId = $_GET['OwnerId'];}
+    if(isset($_GET['Status'])){$Status = $_GET['Status'];}
+    if(isset($_GET['desc_disposition'])){$desc_disposition = $_GET['desc_disposition'];}
+    if(isset($_GET['due_date'])) $due_date = $_GET['due_date'];
+    if(isset($_GET['Subject'])) $Subject = $_GET['Subject'];
+    if(isset($_GET['calltype'])) $calltype = $_GET['calltype'];
+    if(isset($_GET['callattempt'])) $callattempt = $_GET['callattempt'];
+    if($Disposition == "Closed" && isset($_GET['closing_category'])) $closing_category = $_GET['closing_category'];
+    if(isset($_GET['chasedata_id'])) $chasedata_id = $_GET['chasedata_id'];
 
-    $mySforceConnection = new SforceEnterpriseClient();
-    $mySforceConnection->createConnection("soapclient/enterprise.wsdl.xml");
-    $mySforceConnection->setEndpoint('https://login.salesforce.com/services/Soap/c/32.0');
-    
-    $debug = 0;
-    if (isset($_GET['debug'])) $debug = $_GET['debug'];
-    unset($_GET['debug']);
-
-    $host = '';
-    $showlead = 0;
-    if (isset($_GET['showlead'])) $showlead = $_GET['showlead'];
-    unset($_GET['showlead']);
-
-    $consoleview = 0;
-    if (isset($_GET['consoleview'])) $consoleview = $_GET['consoleview'];
-    unset($_GET['consoleview']);
-
-
-    // show array fields
-    function _debug($arr){
-        echo "<pre>";
-        print_r($arr);
-        echo "</pre>";
-    }
-
-    /* get format phone number string */
-    function getPhoneText($numbers){
-        return "(" . substr($numbers,0,3) . ") " . substr($numbers, 3,3) . "-" . substr($numbers, 6);
-    }
-    
-    function getTypes($array, $types){
-        $list = [];
-        foreach ($array as $key => $value) {
-            foreach ($types as $row) {
-                if ($row->name == $key) $list[$key] = $row->type;
-            }
-        }
-        return $list;
-    }
-
-    function adjustData($array, $types){
-        foreach ($array as $key => $value) {
-            if (isset($types[$key]) and $types[$key] == 'datetime'){
-                $array[$key] = date('Y-m-d\TH:i:s\Z', strtotime($value));
-            }
-
-            if (strpos($value, '!TIME_EDT_') !== false){
-                // $array[$key] = gmdate('Y-m-d\TH:i:s\Z', strtotime(str_replace('!TIME_EDT_','',$value)));
-                $date = new DateTime(str_replace('!TIME_EDT_','',$value), new DateTimeZone('America/New_York'));
-                $date->setTimezone(new DateTimeZone('UTC'));
-                $array[$key] = $date->format('Y-m-d\TH:i:s\Z');
-            }else if (strpos($value, '!TIME_CDT_') !== false){
-                // $array[$key] = gmdate('Y-m-d\TH:i:s\Z', strtotime(str_replace('!TIME_CDT_','',$value)));
-                $date = new DateTime(str_replace('!TIME_CDT_','',$value),new DateTimeZone('America/Chicago'));
-                $date->setTimezone(new DateTimeZone('UTC'));
-                $array[$key] = $date->format('Y-m-d\TH:i:s\Z');
-                
-            }else if (strpos($value, '!TIME_MST_') !== false){
-                // $array[$key] = gmdate('Y-m-d\TH:i:s\Z', strtotime(str_replace('!TIME_MST_','',$value)));
-                $date = new DateTime(str_replace('!TIME_MST_','',$value), new DateTimeZone('America/Denver'));
-                $date->setTimezone(new DateTimeZone('UTC'));
-                $array[$key] = $date->format('Y-m-d\TH:i:s\Z');
-            }else if (strpos($value, '!TIME_') !== false){
-                $array[$key] = gmdate('Y-m-d\TH:i:s\Z', strtotime(str_replace('!TIME_','',$value)));
-            }
-        }
-        return $array;
-    }
-
-    function logger($str){
-        $fh = fopen('log.txt', 'a') or die("can't open file");
-        fwrite($fh, $str . "\r\n");
-        fclose($fh);
-    }
-    
-
-    function url_to_domain($url)
-    {
-        $host = @parse_url($url, PHP_URL_HOST);
-        if (substr($host, 0, 4) == "www.")
-            $host = substr($host, 4);
-        return $host;
-    }
-
-
-
-    /*
-     * function to get special values
-     */
-    $specialVals = array(
-        "!DATENOW!" => gmdate("Y-m-d\TH:i:s\Z", strtotime('now')),
-        "!TMRW!"    => gmdate("Y-m-d\TH:i:s\Z", strtotime('+1 day', strtotime('now'))),
-        "!WEEK!"    => gmdate("Y-m-d\TH:i:s\Z", strtotime('+1 week', strtotime('now'))),
-        "!MONTH!"    => gmdate("Y-m-d\TH:i:s\Z", strtotime('+1 month', strtotime('now'))),
-    );
-    function getSpecialVal($name){
-        $specialVals = $GLOBALS['specialVals'];
-        if (array_key_exists($name, $specialVals)){
-            return $specialVals[$name];
-        }
-        return null;
-    }
-
-    if ($debug){
-        logger(date('Y-m-d H:i:s'));
-        logger("--------------------------------------------------------------");
-    }
-
-    ////////////////////////////////////////////////////////////////////////////////////
-
-    $parse_array = [];
-    foreach($_GET as $key => $value) {
-        $checkVal = getSpecialVal($value);
-        if ($checkVal!==null) $parse_array[$key] = $checkVal;
-        else $parse_array[$key] = $value;
-    }
-
-
-    // _debug($parse_array);
-    // exit();
-
-    if (!isset($parse_array['U']) || !isset($parse_array['P']) || !isset($parse_array['Token'])){
-        echo "UserName and PassWord are required!";
-        exit();
-    }
-
-    $is_update = false;
-    
     try {
-        $res = $mySforceConnection->login($parse_array['U'], $parse_array['P'].$parse_array['Token']);
-        $host = url_to_domain($res->serverUrl);
+           
+        define("USERNAME", "g.sandoval@chasedatacorp.com");       
+        define("PASSWORD", "1Nt3gr4t10n!_!");
+        define("SECURITY_TOKEN", "j4RUbBkGl4PosPQOUkqIxwgz0");
         
-        echo "Sucessfully logged in\r\n";
-        if ($debug) logger('Sucessfully logged in');
+    
+        $mySforceConnection = new SforceEnterpriseClient();
+        $mySforceConnection->createConnection("soapclient/enterprise.wsdl.xml");
+        // this sets the endpoint for a sandbox login:
 
-        unset($parse_array['U']);
-        unset($parse_array['P']);
-        unset($parse_array['Token']);
+        // $mySforceConnection->setEndpoint('https://betterhearing.my.salesforce.com/services/Soap/c/32.0');
 
-
-        $meta = $mySforceConnection->describeSObject('Lead')->fields;
-        $types = getTypes($parse_array, $meta);
-        $parse_array = adjustData($parse_array, $types);
-
-        // _debug($parse_array);
-        // exit;
-
-        if (isset($parse_array['SearchType']) && isset($parse_array['SearchValue']) && $parse_array['SearchValue'] != ''){
-            foreach ($meta as $row) {
-                if ($row->name == $parse_array['SearchType']) $type = $row->type;
-            }
-
-            if (!isset($type)){
-                echo "The SearchType is Unknown Field in Lead";
-                if ($debug) logger("The SearchType is Unknown Field in Lead");
-                exit();
-            }
-
-            // special case in Phone
-            // if ($parse_array['SearchType']=='Phone') $parse_array['SearchValue'] = getPhoneText($parse_array['SearchValue']);
-            if (isset($parse_array['removeleading'])){
-                if ($parse_array['removeleading'] != '')
-                    $parse_array['SearchValue'] = substr($parse_array['SearchValue'], $parse_array['removeleading']); 
-                unset($parse_array['removeleading']);
-            }
-
-            if (in_array($type, $numArray))
-                $query = "Select l.Id From Lead l where " . $parse_array['SearchType'] . "=" . $parse_array['SearchValue'] . "";
-            else
-                $query = "Select l.Id From Lead l where " . $parse_array['SearchType'] . "='" . $parse_array['SearchValue'] . "'";
-
-            unset($parse_array['SearchType']);
-            unset($parse_array['SearchValue']);
-            /* Run Query */
-            $leads = $mySforceConnection->query($query);
-            if ($debug) {
-                logger("Query : " . $query);
-                logger(json_encode($leads));
-            }
-            if ($leads->size > 0){
-                $lead = $leads->records[0];
-                $LeadObj = array(
-                    "Id"     => $lead->Id,
-                );
-                $is_update = true;
-            }
+        $mySforceConnection->login(USERNAME, PASSWORD.SECURITY_TOKEN);
+    
+        // print_r($mylogin);
+        if ($Disposition == "Closed" && !isset($closing_category)) {
+            echo "Invalid parameters in Url!"; exit;
         }
-        else{
-            $LeadObj = array();
-        }        
+       
+        if (isset($FirstName) && isset($LastName) && isset($PrimaryPhone) && isset($Disposition) && isset($Notes) && isset($Call_Recording) && isset($WhoId) && isset($CallDurationInSeconds) && isset($Status)  && isset($calltype)  && isset($callattempt)){
+            
+            $Description = $FirstName . " " . $LastName . "\r\nPrimaryPhone:" . $PrimaryPhone . "\r\nDisposition:" . $desc_disposition .  "\r\nNotes:" . $Notes . "\r\nCall Recording:" . $Call_Recording; 
 
-        foreach ($parse_array as $key => $value) {
-            if (strpos($key, 'call_') !== false or $value ==='' or strpos($key, 'event_') !== false) continue;
-            $LeadObj[$key] = $value;
-        }
+            /* first Completed Task */
+            $sObject = array(
+                "Description" => $Description,
+                "CallDurationInSeconds" => $CallDurationInSeconds,
+                "WhoId" => $WhoId,
+                "OwnerId" => $OwnerId,
+                "Status"  => $Status,
+                "Type" => "Call",
+                "Subject" => $calltype . " " . $PrimaryPhone,
+                "Priority" => "Normal",
+                "ActivityDate" => date('Y-m-d hh:mm:ss')
+            );
+            if (!$OwnerId) unset($sObject['OwnerId']);
 
-        if ($debug){
-            logger("Lead Object fields:");
-            logger(json_encode($LeadObj));
-        }
-        
-
-        $sObject = [];
-        foreach ($parse_array as $key => $value) {
-            if (strpos($key, 'call_') !== false) {
-                $sObject[str_replace('call_', '', $key)] = $value;
-                $is_task = true;
-            }
-        }
-
-        _debug($LeadObj);
-
-        /* Update Lead */
-        if ($is_update){
-            echo "*** Updating Lead ...\r\n";
-            if ($debug) logger("*** Updating Lead ...");
-            $updateResponse = $mySforceConnection->update(array($LeadObj), 'Lead');
-            $lead_id = $updateResponse[0]->id;
-            print_r($updateResponse);
-            if ($debug) logger(json_encode($updateResponse));
-
-        }else{
-            echo "*** Creating new Lead ...\r\n";
-            if ($debug) logger("*** Creating new Lead ...");
-            $createResponse = $mySforceConnection->create(array($LeadObj), 'Lead');
-            $lead_id = $createResponse[0]->id;
+            echo "**** Creating the 1st Completed Task:\r\n";
+            $createResponse = $mySforceConnection->create(array($sObject), 'Task');
             print_r($createResponse);
-            if ($debug) logger(json_encode($createResponse));
-        }
-        echo "<br>";
+            echo "<br>"; 
 
-        if ($lead_id == ''){
-            if ($debug) logger("There is error in Lead Pushing");
-            echo "There is error in Lead Pushing"; exit();
-        }
+            /* 2nd Completed Task */
+            $sObject['Subject'] = $desc_disposition . ": " . $callattempt;
+            $sObject['ChaseData_CallDuration__c'] = $CallDurationInSeconds;
+            echo "**** Creating the 2nd Completed Task:\r\n";            
+            $createResponse = $mySforceConnection->create(array($sObject), 'Task');
+            print_r($createResponse);
+            echo "<br>";
 
-        $is_task = false;
-        /* first Completed Task */
-        $sObject = array(
-            "WhoId" => substr($lead_id, 0, strlen($lead_id)-3),
-            "Status"  => "Completed",
-        );
-
-        foreach ($parse_array as $key => $value) {
-            if (strpos($key, 'call_') !== false) {
-                $sObject[str_replace('call_', '', $key)] = $value;
-                $is_task = true;
-            }
-        }
-
-        _debug($sObject);
-        
-        if ($is_task){
-            echo "*** Creating the Completed Task:\r\n";
-            if ($debug) {
-                logger("Task Object fields:");
-                logger("*** Creating the Completed Task");
-                logger(json_encode($sObject));    
+            if (isset($due_date) && isset($Subject)){
+                /* Cretae future Task */
+                $date = strtotime($due_date);
+                $sObject['ActivityDate'] = date('Y-m-d hh:mm:ss',$date);
+                $sObject['Subject'] = $Subject;
+                $sObject['Status'] = 'Not started';
+                unset($sObject['CallDurationInSeconds']);
+                
+                $createResponse = $mySforceConnection->create(array($sObject), 'Task');
+                echo "**** Creating the following:\r\n";
+                print_r($createResponse);
             }            
 
-            $createResponse = $mySforceConnection->create(array($sObject), 'Task');
-            _debug($createResponse);
-            if ($debug) logger(json_encode($createResponse));
+
+            /* Update Lead Status as Disposition */
+            echo "<br><br>**** Lead Update Result <br>";
+
+
+            // if Disposition == "Not Reached" then call apex function
+            if ($Disposition === "Not Reached") {
+                // Define constants for the web service. We'll use these later
+                $parsedURL = parse_url($mySforceConnection->getLocation());
+                define ("_SFDC_SERVER_", substr($parsedURL['host'],0,strpos($parsedURL['host'], '.')));
+                define ("_WS_NAME_", 'Lead_NotReached');
+                define ("_WS_WSDL_", _WS_NAME_ . '.xml');
+                define ("_WS_ENDPOINT_", 'https://' . _SFDC_SERVER_ . '.salesforce.com/services/wsdl/class/' . _WS_NAME_);
+                define ("_WS_NAMESPACE_", 'http://soap.sforce.com/schemas/class/' . _WS_NAME_);
+
+                $client = new SoapClient("soapclient/" . _WS_WSDL_);
+                $sforce_header = new SoapHeader(_WS_NAMESPACE_, "SessionHeader", array("sessionId" => $mySforceConnection->getSessionId()));
+                $client->__setSoapHeaders(array($sforce_header));
+                 
+                $wrkArray = array(
+                                'leadId'=> $WhoId,
+                                'clickedButtonType'=> "NotLVM"
+                            );
+
+                // Call the web service
+                $response = $client->setLeadAsNotReached($wrkArray);
+                // Output results to browser                
+                print_r($response);
+            }
+
+            /* if Disposition is Closed then update Lead with closing category */
+            if ($Disposition == 'Closed'){
+                $LeadObj = array(
+                                    "Id"     => $WhoId,
+                                    "Status" => $Disposition,
+                                    "Lead_Closing_Category__c" => $closing_category,
+                                    "Dialfire_Contact_ID__c" => $chasedata_id,
+                            );
+            }else{
+                $LeadObj = array(
+                                    "Id"     => $WhoId,
+                                    "Status" => $Disposition,
+                                    "Dialfire_Contact_ID__c" => $chasedata_id,
+                            );
+            }
+
+            $updateResponse = $mySforceConnection->update(array($LeadObj), 'Lead');
+            print_r($updateResponse);
         }else{
-            echo "Completed Task was not created because there is not fields";
-            if ($debug) logger("Completed Task was not created because there is not fields");
+            echo "Invalid parameters in Url!";
         }
-        echo "<br>";
-
-
-
-        /*
-        *  ADD EVENT WITH   event_ prefix
-        *  
-        */
-        $is_event = false;
-        $eObject = array(
-             "WhoId" => substr($lead_id, 0, strlen($lead_id)-3),
-        );
-        foreach ($parse_array as $key => $value) {
-            if (strpos($key, 'event_') !== false) {
-                $eObject[str_replace('event_', '', $key)] = $value;
-                $is_event = true;
-            }
-        }
-
-        if ($is_event){
-            echo "*** Creating an Event:\r\n";
-
-            if ($debug){
-                logger("Event Object Fields: ");
-                logger("*** Creating an event...");
-                logger(json_encode($eObject));
-            }
-
-            $eObject['ReminderDateTime'] = gmdate("Y-m-d\TH:i:s\Z", strtotime('-15 minute', strtotime($eObject['StartDateTime'])));
-
-            _debug($eObject);
-
-            $createResponse = $mySforceConnection->create(array($eObject), 'Event');
-            _debug($createResponse);
-        }
-
     }catch (Exception $e) {
         echo $mySforceConnection->getLastRequest();
         echo $e->faultstring;
-        if ($debug) logger($e->faultstring);
     }
-    // download log file
-    if ($debug){
-        logger(' ');
-        echo '<a href="../log.txt" id="download" download></a>';
-        echo '<script>document.getElementById("download").click()</script>';
-    }
-
-    if (isset($lead_id) && isset($host) && $showlead === '1'){
-        if ($consoleview){
-            $url = "https://".$host ."/console#/".$lead_id;
-        }
-        else
-            $url = 'https://' . $host . "/lightning/r/". $lead_id . "/view";
-
-        echo $url;
-        if ($debug) 
-            logger("Redirect Url : " . $url);
-        header('Location:' . $url, true);
-        exit();
-    }
-?>
